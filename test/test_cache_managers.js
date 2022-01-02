7a43a83a0c3442433ba30763510bfe737a2311f5
Thecarisma <azeezadewale98@gmail.com>
2022-01-02 18:19:19 +0100

    create the cache manage for Cookie

--- test/test_cache_managers.js
+++ test/test_cache_managers.js
@@ -19,13 +19,26 @@ const assert = require('assert');
 const utils = require('../lib/utils');
 const FuInterceptor = require('../lib/core/FuInterceptor');
 const MapFFSCacheManager = require('../lib/cache_managers/MapFFSCacheManager');
+const CookieFFSCacheManager = require('../lib/cache_managers/CookieFFSCacheManager');
 const LocalStorageFFSCacheManager = require('../lib/cache_managers/LocalStorageFFSCacheManager');
 const SessionStorageFFSCacheManager = require('../lib/cache_managers/SessionStorageFFSCacheManager');
+
+function decryptor(value, options) {
+	return `{0123456890}_ENCRYPTED`;
+}
+function encryptor(value, options) {
+	return `{0123456890}_ENCRYPTED`;
+}
+
+it('*FFSCacheManager TODO', () => {
+	console.log("TEST REMOVAL OF INTERCEPTORS FROM FuInterceptor using *.interceptorIds");
+});
 
 it('MapFFSCacheManager test', () => {
 	const map = { user: "thecarisma" };
 	const fuInterceptor = new FuInterceptor();
-	const mapFFSCacheManager = new MapFFSCacheManager(fuInterceptor, map);
+	const mapFFSCacheManager = new MapFFSCacheManager(map);
+	mapFFSCacheManager.registerInterceptors(fuInterceptor);
 
 	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_REQUEST", { cache: mapFFSCacheManager }), undefined);
 	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "POST_RESPONSE", { cache: mapFFSCacheManager, response: { status: 200 }}), undefined);
@@ -39,7 +52,8 @@ it('MapFFSCacheManager test', () => {
 
 it('MapFFSCacheManager test Alt', () => {
 	const fuInterceptor = new FuInterceptor();
-	const mapFFSCacheManager = new MapFFSCacheManager(fuInterceptor);
+	const mapFFSCacheManager = new MapFFSCacheManager();
+	mapFFSCacheManager.registerInterceptors(fuInterceptor);
 
 	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_REQUEST", { cache: mapFFSCacheManager }), undefined);
 	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "POST_RESPONSE", { cache: mapFFSCacheManager, response: { status: 200 }}), undefined);
@@ -50,7 +64,8 @@ it('MapFFSCacheManager test Alt', () => {
 it('LocalStorageFFSCacheManager test', () => {
 	const fuInterceptor = new FuInterceptor();
 	localStorage.removeItem("undefined_SINGLE");
-	const lsFFSCacheManager = new LocalStorageFFSCacheManager(fuInterceptor, null, null, localStorage);
+	const lsFFSCacheManager = new LocalStorageFFSCacheManager(null, null, localStorage);
+	lsFFSCacheManager.registerInterceptors(fuInterceptor);
 
 	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_REQUEST", { cache: lsFFSCacheManager }), undefined);
 	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_RESPONSE", { cache: lsFFSCacheManager, response: "{ status: 200 }"}), undefined);
@@ -60,15 +75,10 @@ it('LocalStorageFFSCacheManager test', () => {
 });
 
 it('LocalStorageFFSCacheManager test with encryptor and decryptor', () => {
-	function decryptor(value, options) {
-		return `{0123456890}_ENCRYPTED`;
-	}
-	function encryptor(value, options) {
-		return `{0123456890}_ENCRYPTED`;
-	}
 	const fuInterceptor = new FuInterceptor();
 	localStorage.removeItem("undefined_SINGLE");
-	const lsFFSCacheManager = new LocalStorageFFSCacheManager(fuInterceptor, decryptor, encryptor, localStorage);
+	const lsFFSCacheManager = new LocalStorageFFSCacheManager(decryptor, encryptor, localStorage);
+	lsFFSCacheManager.registerInterceptors(fuInterceptor);
 
 	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_REQUEST", { cache: lsFFSCacheManager }), undefined);
 	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_RESPONSE", { cache: lsFFSCacheManager, response: "{ status: 200 }"}), undefined);
@@ -80,7 +90,8 @@ it('LocalStorageFFSCacheManager test with encryptor and decryptor', () => {
 it('SessionStorageFFSCacheManager test', () => {
 	const fuInterceptor = new FuInterceptor();
 	localStorage.removeItem("undefined_SINGLE");
-	const ssFFSCacheManager = new SessionStorageFFSCacheManager(fuInterceptor, null, null, localStorage);
+	const ssFFSCacheManager = new SessionStorageFFSCacheManager(null, null, localStorage);
+	ssFFSCacheManager.registerInterceptors(fuInterceptor);
 
 	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_REQUEST", { cache: ssFFSCacheManager }), undefined);
 	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_RESPONSE", { cache: ssFFSCacheManager, response: "{ status: 200 }"}), undefined);
@@ -90,21 +101,50 @@ it('SessionStorageFFSCacheManager test', () => {
 });
 
 it('SessionStorageFFSCacheManager test with encryptor and decryptor', () => {
-	function decryptor(value, options) {
-		return `{0123456890}_ENCRYPTED`;
-	}
-	function encryptor(value, options) {
-		return `{0123456890}_ENCRYPTED`;
-	}
 	const fuInterceptor = new FuInterceptor();
 	localStorage.removeItem("undefined_SINGLE");
-	const ssFFSCacheManager = new SessionStorageFFSCacheManager(fuInterceptor, decryptor, encryptor, localStorage);
+	const ssFFSCacheManager = new SessionStorageFFSCacheManager(decryptor, encryptor, localStorage);
+	ssFFSCacheManager.registerInterceptors(fuInterceptor);
 
 	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_REQUEST", { cache: ssFFSCacheManager }), undefined);
 	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_RESPONSE", { cache: ssFFSCacheManager, response: "{ status: 200 }"}), undefined);
 	assert.notEqual(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_REQUEST", { cache: ssFFSCacheManager }), undefined);
 	assert.deepEqual(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_REQUEST", { cache: ssFFSCacheManager }), decryptor("{ status: 200 }"));
 	assert.deepEqual(localStorage.getItem("undefined_SINGLE"), decryptor("{ status: 200 }"));
+});
+
+it('CookieFFSCacheManager test', () => {
+	const fuInterceptor = new FuInterceptor();
+	localStorage.removeItem("undefined_SINGLE");
+	const cookieFFSCacheManager = new CookieFFSCacheManager(null, null, null, localStorage);
+	cookieFFSCacheManager.registerInterceptors(fuInterceptor);
+
+	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_REQUEST", { cache: cookieFFSCacheManager }), undefined);
+	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_RESPONSE", { cache: cookieFFSCacheManager, response: { status: 200 }}), undefined);
+	assert.notEqual(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_REQUEST", { cache: cookieFFSCacheManager }), undefined);
+	assert.deepEqual(localStorage.getItem("undefined_SINGLE"), JSON.stringify({
+		name: "undefined_SINGLE",
+		expires: "",
+		value: { status: 200 },
+		path: "/"
+	}));
+});
+
+it('CookieFFSCacheManager test with encryptor and decryptor', () => {
+	const fuInterceptor = new FuInterceptor();
+	localStorage.removeItem("undefined_SINGLE");
+	const cookieFFSCacheManager = new CookieFFSCacheManager(decryptor, encryptor, null, localStorage);
+	cookieFFSCacheManager.registerInterceptors(fuInterceptor);
+
+	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_REQUEST", { cache: cookieFFSCacheManager }), undefined);
+	assert.equal(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_RESPONSE", { cache: cookieFFSCacheManager, response: "{ status: 200 }"}), undefined);
+	assert.notEqual(utils.invokeForEachInterceptorType(fuInterceptor, "PRE_REQUEST", { cache: cookieFFSCacheManager }), undefined);
+	assert.deepEqual(localStorage.getItem("undefined_SINGLE"), JSON.stringify({
+		name: "undefined_SINGLE",
+		expires: "",
+		value: decryptor("{ status: 200 }", {}),
+		path: "/"
+	}));
 });
 
 it('RedisFFSCacheManager test', () => {
